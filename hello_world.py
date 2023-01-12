0472455ba72fe4d6df37d0533338fa59fad8a206
Areeeb98 <122514572+Areeeb98@users.noreply.github.com>
2023-01-12 12:42:33 +0530

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,26 @@
+'''
+Day: Wednesday
+Date: 19-10-2022
+Time: 12PM
+Topic: Type methods (int, float), comments
+'''
+# print('hello world')
+# print('hello world')
+# print('hello world')
+# print('hello world')
+
+# print(type(4))
+# print(type(-8))
+# print(type(0))
+# print(type(345686))
+
+#print(3)
+# print(7)
+# print(9)
+
+# print(type(-3.4))
+# print(type(.6))
+# print(type(0.9))
+# print(type(3232.59))
+
+print('hello world')
